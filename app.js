e87a473ab8b31ef4b67820cf2ccba4a79e376e22
wyixin <single.w.1992@gmail.com>
2014-11-17 16:14:13 +0800

    login papge & express session setup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 var expressLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ resave: false, saveUninitialized: false, secret: 'ssbywyx' })); 
 // setup static file server
 app.use(express.static(__dirname + '/public'));
 
@@ -10,9 +16,47 @@ app.use(expressLayouts);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+// dummy data
+var users = {
+    tj: {name: 'tj', salt: 'f4d2'} //password
+};
+
+// use defined functions
+
+function authenticate(name, pass, fn) {
+    if (!module.parent) console.log('authenticating %s:%s', name, pass);
+    var user = users[name];
+    // query the db for the given username
+    if (!user) return fn(new Error('cannot find user'));
+    return fn(null, user);
+};
+
+
+
 app.get('/', function (req, res) {
     res.render('pages/index', { title: 'Express' });
 });
+
+app.get('/login', function(req, res) {
+    if(req.session.user) {
+        res.redirect('/index'); // redirect to main page if login
+    } else {
+        res.render('pages/login');
+    }
+});
+
+app.post('/login', function(req, res) {
+    authenticate(req.body.username, req.body.password, function(err, user) {
+        if(user) {
+            req.session.user = user;
+        } else {
+            res.redirect('/login');
+        }
+    });
+    res.end();
+});
+
 
 var server = app.listen(3000, function () {
 
