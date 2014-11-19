edb9681da0a7c49c0a262126b34f598f140e90de
wyixin <single.w.1992@gmail.com>
2014-11-19 15:14:25 +0800

    add regist static page & user auth, create user function

--- app/server/modules/db.js
+++ app/server/modules/db.js
@@ -4,7 +4,7 @@ var Server = require('mongodb').Server;
 
 var dbPort = 27017;
 var dbHost = '202.114.177.31';
-var dbName = 'test';
+var dbName = 'sales';
 
 /* establish the database connection */
 var db = new MongoDB(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
@@ -16,6 +16,46 @@ db.open(function(e, d) {
     }
 });
 
-var testData = db.collection('testData');
-console.log(testData);
-//console.log(testData.findOne());
+// users model
+var users = db.collection('users');
+
+var md5 = function(str) {
+    return crypto.createHash('md5').update(str).digest('hex');
+};
+
+var passwdHash = function(str) {
+    return md5(str);
+};
+
+exports.authenticate = function(email, pass, cb) {
+    users.findOne({email: email}, function(e, u) {
+        if(u) {
+            if(u.password === passwdHash(pass)) {
+                cb(null, u);
+            } else {
+                cb('password error');            
+            }
+        } else {
+            cb('user not exist');            
+        }
+    });
+};
+
+//data {username: "xxx", password: "xxx", email: "xxx"}
+exports.createAccount = function (data, cb) {
+    users.findOne({username: data.username}, function(e, u){
+        if(u) {
+            cb('username token');
+        } else {
+            users.findOne({email: data.email}, function(e, u){
+                if(u) {
+                    cb('email token');
+                } else {
+                    data.password = passwdHash(data.password);
+                    users.insert(data, {safe: true}, cb);
+                }
+            });
+        }
+    });
+};
+
--- app/server/router.js
+++ app/server/router.js
@@ -1,21 +1,6 @@
-
+var db = require('./modules/db');
 
 function route(app) {
-    // dummy data
-    var users = {
-        tj: {name: 'tj', salt: 'f4d2'} //password
-    };
-
-    // use defined functions
-
-    function authenticate(name, pass, fn) {
-        if (!module.parent) console.log('authenticating %s:%s', name, pass);
-        var user = users[name];
-        // query the db for the given username
-        if (!user) return fn(new Error('cannot find user'));
-        return fn(null, user);
-    };
-
 
     app.get('/', function (req, res) {
         res.render('pages/index', { title: 'Express' });
@@ -36,7 +21,7 @@ function route(app) {
     });
 
     app.post('/login', function(req, res) {
-        authenticate(req.body.username, req.body.password, function(err, user) {
+        db.authenticate(req.body.username, req.body.password, function(err, user) {
             if(user) {
                 req.session.user = user;
                 res.redirect('/');
